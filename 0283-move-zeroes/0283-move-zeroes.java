f75958862c4eedd0028991cb9c8fd174565c6e4d
Sanidhy Chaturvedi <sanidhy.25bcon1241@jecrcu.edu.in>
2026-03-21 10:35:37 +0530

    Time: 2 ms (90.55%), Space: 48.1 MB (15.13%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int ptr = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[ptr] = nums[i];
+                ptr++;
+            }
+        }
+
+        for (int j = ptr; j < nums.length; j++) {
+            nums[j] = 0;
+        }
+    }
+}
