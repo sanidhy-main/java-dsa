56a88437b9fed0f99c6f2f78e730ec6bed1dfa96
Sanidhy Chaturvedi <sanidhy.25bcon1241@jecrcu.edu.in>
2026-03-22 22:49:45 +0530

    Time: 9 ms (37.38%), Space: 49.2 MB (51.65%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,24 +1,24 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        int[][] currentInterval = new int[1][2];
-        int[][] mergedIntervals = new int[intervals.length][2];
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int[] currentInterval = intervals[0];
+        int[][] result  = new int[intervals.length][2];
         int counter = 0;
 
-        currentInterval[0] = intervals[0];
-        
-        for (int i = 0; i < intervals.length - 1; i++) {
-        if (currentInterval[0][1] >= intervals[i + 1][0]) {
-            currentInterval[0][1] = Math.max(currentInterval[0][1], intervals[i+1][1]);
-        } else {
-            mergedIntervals[counter] = currentInterval[0];
-            currentInterval[0] = intervals[i + 1];
-            counter += 1;
-        }
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] <= currentInterval[1]) {
+                currentInterval[1] = Math.max(intervals[i][1], currentInterval[1]);
+            } else {
+                result[counter] = Arrays.copyOf(currentInterval, 2);
+                counter++;
+                currentInterval = intervals[i];
+            }
         }
 
-        mergedIntervals[counter] = currentInterval[0];
+        result[counter] = Arrays.copyOf(currentInterval, 2);
+        counter++;
 
-        return Arrays.copyOfRange(mergedIntervals, 0, counter + 1);
+        return Arrays.copyOf(result, counter);
     }
 }
