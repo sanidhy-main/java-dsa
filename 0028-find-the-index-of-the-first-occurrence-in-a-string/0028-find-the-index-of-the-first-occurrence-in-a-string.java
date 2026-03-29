ce476f64d14582d5e6b58e1543dc9cb233f0882b
Sanidhy Chaturvedi <sanidhy.25bcon1241@jecrcu.edu.in>
2026-03-29 17:58:52 +0530

    Time: 1 ms (46.4%), Space: 43.1 MB (45.82%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,14 +1,12 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        int result = -1;
-
         if (needle.length() == 0) {
             return 0;
         }
-        
+
         for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            int j = 1;
             if (haystack.charAt(i) == needle.charAt(0)) {
-                int j;
                 for (j = 1; j < needle.length(); j++) {
                     if (haystack.charAt(i + j) != needle.charAt(j)) {
                         break;
@@ -18,10 +16,9 @@ class Solution {
                 if (j == needle.length()) {
                     return i;
                 }
-            } else {
-                continue;
             }
         }
-    return result;
+
+        return -1;
     }
 }
