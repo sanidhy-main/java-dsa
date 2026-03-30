39ecd1693192074a19b85f67a79bf222e1f45cfc
Sanidhy Chaturvedi <sanidhy.25bcon1241@jecrcu.edu.in>
2026-03-30 22:41:58 +0530

    Time: 21 ms (8.16%), Space: 47.4 MB (6.75%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -6,30 +6,29 @@ class Solution {
             return false;
         }
 
-        int len = s.length();
-
-        for (int i = 0; i < len; i++) {
-            if (!map.containsKey(s.charAt(i))) {
-                map.put(s.charAt(i), 1);
-            } else {
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
                 map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
-            }
-        }
-
-        for (int j = 0; j < len; j++) {
-            if (!map.containsKey(t.charAt(j))) {
-                return false;
             } else {
-                map.put(t.charAt(j), map.get(t.charAt(j)) - 1);
+                map.put(s.charAt(i), 1);
             }
         }
 
-        for (int val : map.values()) {
-            if (val != 0) {
+        for (int j = 0; j < t.length(); j++) {
+            if (map.containsKey(t.charAt(j))) {
+                map.put(t.charAt(j), map.get(t.charAt(j)) - 1);
+            } else {
+                map.put(t.charAt(j), 1);
+            }
+        }
+
+        for (int value : map.values()) {
+            if (value != 0) {
                 return false;
             }
         }
 
         return true;
+
     }
 }
