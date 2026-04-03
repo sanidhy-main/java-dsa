0f1298b8b5d86a88bb22524f24ab396b7b963fef
Sanidhy Chaturvedi <sanidhy.25bcon1241@jecrcu.edu.in>
2026-04-03 22:58:45 +0530

    Time: 19 ms (10.72%), Space: 47 MB (6.53%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,34 +1,35 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character, Integer> map = new HashMap<>();
+       if (s.length() != t.length()) {
+        return false;
+       }
 
-        if (s.length() != t.length()) {
+       HashMap<Character, Integer> map = new HashMap<>();
+
+       int len = s.length();
+
+       for (int i = 0; i < len; i++) {
+        if (!map.containsKey(s.charAt(i))) {
+            map.put(s.charAt(i), 1);
+        } else {
+            map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
+        }
+       }
+
+       for (int j = 0; j < len; j++) {
+        if (!map.containsKey(t.charAt(j))) {
+            return false;
+        } else {
+            map.put(t.charAt(j), map.get(t.charAt(j)) - 1);
+        }
+       }
+
+       for (int value : map.values()) {
+        if (value != 0) {
             return false;
         }
+       }
 
-        for (int i = 0; i < s.length(); i++) {
-            if (map.containsKey(s.charAt(i))) {
-                map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
-            } else {
-                map.put(s.charAt(i), 1);
-            }
-        }
-
-        for (int j = 0; j < t.length(); j++) {
-            if (map.containsKey(t.charAt(j))) {
-                map.put(t.charAt(j), map.get(t.charAt(j)) - 1);
-            } else {
-                map.put(t.charAt(j), 1);
-            }
-        }
-
-        for (int value : map.values()) {
-            if (value != 0) {
-                return false;
-            }
-        }
-
-        return true;
-
+       return true;
     }
 }
