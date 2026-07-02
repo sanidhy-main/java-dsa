e5818862311e57f8a78a22977df2250a2e9c9f5b
Sanidhy Chaturvedi <sanidhy.25bcon1241@jecrcu.edu.in>
2026-07-02 14:29:50 +0400

    Time: 0 ms (100%), Space: 43.5 MB (59.99%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+        for (int num : nums) {
+            if (nums[mid] == 0) {
+                int temp1 = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp1;
+
+                mid++;
+                low++;
+            } else if (nums[mid] == 2) {
+                int temp2 = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp2;
+
+                high--;
+            } else {
+                mid++;
+            }
+        }
+    }
+}
