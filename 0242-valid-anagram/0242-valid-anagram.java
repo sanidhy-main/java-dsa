1936ba3b986346d19c0727b25a73d125efa678fd
Sanidhy Chaturvedi <sanidhy.25bcon1241@jecrcu.edu.in>
2026-07-11 13:29:31 +0400

    Time: 16 ms (20.93%), Space: 46.6 MB (16.18%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -5,16 +5,21 @@ class Solution {
         }
 
         HashMap<Character, Integer> sMap = new HashMap<>();
-        HashMap<Character, Integer> tMap = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
             sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i), 0) + 1);
         }
 
-        for (int i = 0; i < t.length(); i++) {
-            tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i), 0) + 1);
+        for (int j = 0; j < t.length(); j++) {
+            sMap.put(t.charAt(j), sMap.getOrDefault(t.charAt(j), 0) - 1);
         }
 
-        return sMap.equals(tMap);
+        for (int count : sMap.values()) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
