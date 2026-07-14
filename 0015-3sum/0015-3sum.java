af55385db3d4a2e9dc58f4cc0d9cfc4a8c4f79df
Sanidhy Chaturvedi <sanidhy.25bcon1241@jecrcu.edu.in>
2026-07-14 16:55:01 +0400

    Time: 29 ms (93.75%), Space: 60.3 MB (12.2%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,45 +1,46 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-       Arrays.sort(nums);
+        int left = 0;
+        int middle = 1;
+        int right = nums.length - 1;
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
 
-       List<List<Integer>> li = new ArrayList<>();
-
-        for (int left = 0; left < nums.length - 2; left++) {
-            int ptr1 = left + 1;
-            int ptr2 = nums.length - 1;
-
-            if (left > 0 && nums[left] == nums[left - 1]) {
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
                 continue;
             }
 
-            while (ptr1 < ptr2) {
-                if (nums[left] + nums[ptr1] + nums[ptr2] == 0) {
-                    List<Integer> result = new ArrayList<>();
+            left = i;
+            middle = left + 1;
+            right = nums.length - 1;
 
-                    result.add(nums[left]);
-                    result.add(nums[ptr1]);
-                    result.add(nums[ptr2]);
+            while (middle < right) {
+                if (nums[left] + nums[middle] + nums[right] > 0) {
+                    right--;
+                } else if (nums[left] + nums[middle] + nums[right] < 0) {
+                    middle++;
+                } else {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[left]);
+                    list.add(nums[middle]);
+                    list.add(nums[right]);
+                    
+                    result.add(list);
 
-                    li.add(result);
+                    middle++;
+                    right--;
 
-                    ptr1++;
-                    ptr2--;
-
-                    while (ptr1 < ptr2 && nums[ptr1] == nums[ptr1 - 1]) {
-                        ptr1++;
+                    while (middle < right && nums[middle] == nums[middle-1]) {
+                        middle++;
                     }
 
-                    while (ptr1 < ptr2 && nums[ptr2] == nums[ptr2 + 1]) {
-                        ptr2--;
+                    while (right > left && nums[right] == nums[right+1]) {
+                        right--;
                     }
-
-                } else if (nums[left] + nums[ptr1] + nums[ptr2] < 0) {
-                    ptr1++;
-                } else if (nums[left] + nums[ptr1] + nums[ptr2] > 0) {
-                    ptr2--;
                 }
             }
         }
-        return li;
+        return result;
     }
 }
