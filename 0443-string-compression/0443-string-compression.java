d59fb214aca2468d163654251e31e56f0846bc69
Sanidhy Chaturvedi <sanidhy.25bcon1241@jecrcu.edu.in>
2026-07-14 15:33:26 +0400

    Time: 1 ms (99.9%), Space: 45.4 MB (48.97%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -3,29 +3,27 @@ class Solution {
         int read = 0;
         int write = 0;
 
-        while (read < chars.length) {
-            int count = 1; //reset number of characters to 1
-            while (read + 1 < chars.length && chars[read] == chars[read+1]) { //read ends at last char in a sequence
-                read++; //read forward
-                count++; //add to count
+        while (read < chars.length && write < chars.length) {
+            int count = 1;
+
+            while (read + 1 < chars.length && chars[read] == chars[read+1]) {
+                count++;
+                read++;
             }
 
-            if (write < chars.length) {
-                chars[write++] = chars[read]; //writes the character in its position, and moves forward
-                if (count > 1) {
-                    String s = Integer.toString(count); //converts count to string
+            chars[write++] = chars[read];
+            read++;
 
-                    for (char c : s.toCharArray()) {
-                        chars[write++] = c; //writes the count of the character, and moves forward
-                    }
+            if (count > 1) {
+                String num = Integer.toString(count);
+                int i = 0;
+                while (i < num.length() && write < chars.length) {
+                    chars[write++] = num.charAt(i);
+                    i++;
                 }
             }
-
-            read++; //moves read to the first character of the next sequence
         }
 
-    return write; //returns array till the last character written
+        return write;
     }
 }
-
-//this took me so long, oh god
