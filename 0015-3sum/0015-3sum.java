7b201440690b88b8b87c6ef8eccbb7d2c3998b84
Sanidhy Chaturvedi <sanidhy.25bcon1241@jecrcu.edu.in>
2026-07-16 15:05:14 +0400

    Time: 29 ms (93.77%), Space: 60 MB (15.5%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,46 +1,46 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         int left = 0;
-        int middle = 1;
         int right = nums.length - 1;
+        int middle = 1;
         List<List<Integer>> result = new ArrayList<>();
+
         Arrays.sort(nums);
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) {
+        for (left = 0; left < nums.length - 2; left++) {
+            if (left > 0 && nums[left] == nums[left-1]) {
                 continue;
             }
 
-            left = i;
-            middle = left + 1;
+            middle = left+1;
             right = nums.length - 1;
 
             while (middle < right) {
-                if (nums[left] + nums[middle] + nums[right] > 0) {
-                    right--;
-                } else if (nums[left] + nums[middle] + nums[right] < 0) {
+                if (nums[left] + nums[middle] + nums[right] < 0) {
                     middle++;
+                } else if (nums[left] + nums[middle] + nums[right] > 0) {
+                    right--;
                 } else {
                     List<Integer> list = new ArrayList<>();
                     list.add(nums[left]);
                     list.add(nums[middle]);
                     list.add(nums[right]);
-                    
+
                     result.add(list);
 
                     middle++;
-                    right--;
-
                     while (middle < right && nums[middle] == nums[middle-1]) {
                         middle++;
                     }
 
-                    while (right > left && nums[right] == nums[right+1]) {
-                        right--;
+                    right--;
+                    while (middle < right && nums[right] == nums[right+1]) {
+                    right--;
                     }
                 }
             }
         }
+
         return result;
     }
 }
