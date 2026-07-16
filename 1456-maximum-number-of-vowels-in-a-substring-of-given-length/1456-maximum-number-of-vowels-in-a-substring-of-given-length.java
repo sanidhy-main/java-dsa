672afb93171fc5f77c2a2a4534d9879bae1b5ff9
Sanidhy Chaturvedi <sanidhy.25bcon1241@jecrcu.edu.in>
2026-07-16 15:46:31 +0400

    Time: 24 ms (11.64%), Space: 47.2 MB (5.43%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -4,14 +4,9 @@ class Solution {
         int right = left + k;
         int count = 0;
         int maxCount = Integer.MIN_VALUE;
-        ArrayList<Character> chars = new ArrayList<>();
-        
-        for (int i = 0; i < right; i++) {
-            chars.add(s.charAt(i));
-        }
 
-        for (int x = 0; x < chars.size(); x++) {
-            if (chars.get(x) == 'a' || chars.get(x) == 'e' || chars.get(x) == 'i' || chars.get(x) == 'o' || chars.get(x) == 'u') {
+        for (int x = 0; x < right; x++) {
+            if (s.charAt(x) == 'a' || s.charAt(x) == 'e' || s.charAt(x) == 'i' || s.charAt(x) == 'o' || s.charAt(x) == 'u') {
                 count++;
             }
         }
@@ -19,19 +14,16 @@ class Solution {
         maxCount = Math.max(count, maxCount);
 
         while (left < right && right < s.length()) {
-            chars.remove(left);
             if (s.charAt(left) == 'a' || s.charAt(left) == 'e' || s.charAt(left) == 'i' || s.charAt(left) == 'o' || s.charAt(left) == 'u') {
                 count--;
             }
 
             left++;
 
-            chars.add(s.charAt(right));
             if (s.charAt(right) == 'a' || s.charAt(right) == 'e' || s.charAt(right) == 'i' || s.charAt(right) == 'o' || s.charAt(right) == 'u') {
                 count++;
             }
 
-            chars.add(s.charAt(right));
             right++;
 
             maxCount = Math.max(count, maxCount);
