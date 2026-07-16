24b5c527ed16bf882f61c39d3db5fd1607ea750d
Sanidhy Chaturvedi <sanidhy.25bcon1241@jecrcu.edu.in>
2026-07-16 17:03:33 +0400

    Time: 74 ms (5.49%), Space: 46.9 MB (24.9%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int left = 0;
+        int right = 0;
+        int maxFreq = 0;
+        int maxLen = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        while (right < s.length()) {
+            map.put(s.charAt(right), map.getOrDefault(s.charAt(right), 0) + 1);
+            right++;
+
+            maxFreq = 0;
+            for (int freq : map.values()) {
+                maxFreq = Math.max(freq, maxFreq);
+            }
+
+            while (right - left - maxFreq > k) {
+                map.put(s.charAt(left), map.get(s.charAt(left)) - 1);
+                left++;
+
+                maxFreq = 0;
+                for (int freq : map.values()) {
+                    maxFreq = Math.max(freq, maxFreq);
+                }
+            }
+
+            maxLen = Math.max(maxLen, right - left);
+        }
+
+        return maxLen;
+    }
+}
