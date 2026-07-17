0ac29e7457a73baca8fefcac976ce1fd8b67803e
Sanidhy Chaturvedi <sanidhy.25bcon1241@jecrcu.edu.in>
2026-07-17 14:18:12 +0400

    Time: 22 ms (20.3%), Space: 46.5 MB (49.78%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,34 +1,41 @@
 class Solution {
     public int maxVowels(String s, int k) {
         int left = 0;
-        int right = left + k;
+        int right = k;
+        int maxLen = 0;
         int count = 0;
-        int maxCount = Integer.MIN_VALUE;
 
-        for (int x = 0; x < right; x++) {
-            if (s.charAt(x) == 'a' || s.charAt(x) == 'e' || s.charAt(x) == 'i' || s.charAt(x) == 'o' || s.charAt(x) == 'u') {
+        HashSet<Character> set = new HashSet<>();
+        set.add('a');
+        set.add('e');
+        set.add('i');
+        set.add('o');
+        set.add('u');
+
+        for (int i = 0; i < right; i++) {
+            if (set.contains(s.charAt(i))) {
                 count++;
             }
         }
 
-        maxCount = Math.max(count, maxCount);
+        maxLen = Math.max(maxLen, count);
 
-        while (left < right && right < s.length()) {
-            if (s.charAt(left) == 'a' || s.charAt(left) == 'e' || s.charAt(left) == 'i' || s.charAt(left) == 'o' || s.charAt(left) == 'u') {
-                count--;
-            }
-
-            left++;
-
-            if (s.charAt(right) == 'a' || s.charAt(right) == 'e' || s.charAt(right) == 'i' || s.charAt(right) == 'o' || s.charAt(right) == 'u') {
+        while (right < s.length()) {
+            if (set.contains(s.charAt(right))) {
                 count++;
             }
 
             right++;
 
-            maxCount = Math.max(count, maxCount);
+            if (set.contains(s.charAt(left))) {
+                count--;
+            }
+
+            left++;
+
+            maxLen = Math.max(maxLen, count);
         }
 
-        return maxCount;
+        return maxLen;
     }
 }
