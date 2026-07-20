775d7e2b2c46f4506bf6a4388bb1b35cf7e3aa07
Sanidhy Chaturvedi <sanidhy.25bcon1241@jecrcu.edu.in>
2026-07-20 18:57:06 +0400

    Time: 14 ms (50.63%), Space: 41.9 MB (87.26%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -3,13 +3,12 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int left = 1;
+        int left = 0;
         int right = n;
         int mid = left + (right - left)/2;
 
         while (left <= right) {
             mid = left + (right - left)/2;
-
             if (isBadVersion(mid)) {
                 right = mid - 1;
             } else {
