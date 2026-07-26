8afadeaad075bfb5078c7529b7e7e54486fd1655
Sanidhy Chaturvedi <sanidhy.25bcon1241@jecrcu.edu.in>
2026-07-26 14:02:08 +0400

    Time: 10 ms (84.09%), Space: 50.3 MB (38.17%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,39 +1,40 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int left = 0;
-        int right = 0;
-        int mid = left + (right - left)/2;
-        int answer = 0;
-        int currentLoad = 0;
-        int daysNeeded = 1;
+       int left = 0;
+       int right = 0;
+       int answer = 0;
+       int currentLoad = 0;
+       int daysNeeded = 1;
 
-        for (int w : weights) {
-            left = Math.max(w, left);
-            right += w;
-        }
+       for (int w : weights) {
+        left = Math.max(w, left);
+        right += w;
+       }
 
-        while (left <= right) {
-            mid = left + (right - left)/2;
-            currentLoad = 0;
-            daysNeeded = 1;
+       int mid = left + (right - left)/2;
 
-            for (int weight : weights) {
-                if (currentLoad + weight <= mid) {
-                    currentLoad += weight;
-                } else {
-                    daysNeeded++;
-                    currentLoad = weight;
-                }
-            }
+       while (left <= right) {
+        mid = left + (right - left)/2;
 
-            if (daysNeeded > days) {
-                left = mid + 1;
+        currentLoad = 0;
+        daysNeeded = 1;
+        for (int weight : weights) {
+            if (currentLoad + weight <= mid) {
+                currentLoad += weight;
             } else {
-                answer = mid;
-                right = mid - 1;
+                daysNeeded++;
+                currentLoad = weight;
             }
         }
 
-        return answer;
+        if (daysNeeded <= days) {
+            answer = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+       }
+
+       return answer;
     }
 }
