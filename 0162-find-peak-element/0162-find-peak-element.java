9ccb0944f764e1a00ea00900b675c944aeb9905c
Sanidhy Chaturvedi <sanidhy.25bcon1241@jecrcu.edu.in>
2026-08-01 16:46:42 +0400

    Time: 0 ms (100%), Space: 44.3 MB (37.78%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -5,19 +5,18 @@ class Solution {
         int mid = left + (right - left) / 2;
         int answer = 0;
 
-        while (left <= right) {
+        while (left < right) {
             mid = left + (right - left) / 2;
 
             if (mid < nums.length - 1 && nums[mid] < nums[mid + 1]) {
                 left = mid + 1;
             } else if (mid < nums.length - 1 && nums[mid] > nums[mid+1]) {
-                answer = mid;
-                right = mid - 1;
+                right = mid;
             } else {
                 return mid;
             }
         }
 
-        return answer;   
+        return left;   
     }
 }
